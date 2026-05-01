8191fc3c28b9873d93e8bdda1e8ff804b777309e
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-01 18:51:07 +0530

    fibi

--- num/fibi.py
+++ num/fibi.py
@@ -19,9 +19,23 @@ for i in range(6):
 """
 
 
-a=20
-b=30
+"""
+n=int(input("num :"))
 
-a,b = b,a
 
-print(a,b)
+a=0
+b=1
+for i in range(0,n+1):
+    print(a,end=" ")
+
+    c=a+b
+
+    a=b
+    b=c"""
+
+a,b=0,1
+
+for i in range(10):
+    a,b=b,a+b
+
+    print(a,end=" ")
